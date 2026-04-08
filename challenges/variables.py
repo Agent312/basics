6593cc13ffe7c3ed295f0e50243ca41a47bd6ccf
Agent312 <dluzak.damian05@gmail.com>
2026-04-08 14:38:24 +0200

    :)

---
+++ challenges/variables.py
@@ -0,0 +1,21 @@
+
+# Zapisz zmienne z identyfikatorami które najlepiej
+# opisują wiadomość email np tytuł, treść, nadawca itd.
+# Zapisz przy okazji przykładowe wartości w tych zmiennych
+# Wyświetl te zmienne w konsoli za pomoca funkcji print()
+
+title = "Another email"
+content = "Talk to the hand!"
+sender = "Arnold"
+receiver = "Andy"
+emailDate = "2026.4.09"
+numAttachments = 1
+attachmentName = "normallyitdoesnttakethatlong.doc"
+
+print("Tytuł maila:" + title)
+print("Treść emaila:", content)
+print("Nadawca:", sender)
+print("Odbiorca:", receiver)
+print("Data maila:", emailDate)
+print("Ilość załączników:", numAttachments)
+print("Załącznik nr.[który]:", attachmentName)
