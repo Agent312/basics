da500f6e894511017632908358499e2b7ae38d4e
Agent312 <dluzak.damian05@gmail.com>
2026-07-01 18:41:11 +0200

    another one

---
+++ 04 typ bool .py
@@ -0,0 +1,12 @@
+t = True
+f = False
+o = int(input("podaj pierwszą liczbę: "))
+print(type(t))
+if o > 1000:
+    print("thats a big BOI!")
+w = int(input("podaj drugą liczbę: "))
+print ("liczba podana pierwsza"" > "+"Liczba podana druga")
+print ((o) > (w))
+print ("liczba podana pierwsza"+" < "+"Liczba podana druga")
+print ((o) < (w))
+
