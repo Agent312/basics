2c249760277e3c0b7127c81d21276885473ae1d5
Agent312 <dluzak.damian05@gmail.com>
2026-07-01 15:09:11 +0200

    some new stuff got added :3

---
+++ challenges/strings.py
@@ -0,0 +1,5 @@
+name = str(input("Podaj swoje imię:"))
+surname = str(input("Podaj swoje nazwisko:"))
+city = str(input("Podaj swoje miasto zamieszkania:"))
+
+print("Nazywasz się: "+(name) + " "+ (surname) + " i mieszkasz (w) " + (city))
